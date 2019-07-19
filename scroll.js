c50573bfa86695fd16d45a445c5fd9cf78432776
Sean Barker <imseanbarker@gmail.com>
2019-07-18 22:43:03 -0400

    Update scroll.js

--- scroll.js
+++ scroll.js
@@ -2,11 +2,13 @@
 // Date:   July 12, 2019
 // Description: Portfolio scroll on Nav
 
+//Function to scroll to page on click
 $("a").click(function() {
   var pageId = $(this).attr("data-page");
   $("html, body").animate({ scrollTop: $("#" + pageId).offset().top }, 1000);
 });
 
+// Function to hide the Hero Text when nav is open
 function clickedIt() {
   var canSee = $("#navbarSupportedContent").is(":visible");
   if (canSee != true) {
